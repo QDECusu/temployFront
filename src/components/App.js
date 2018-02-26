a40288b0e1cc2cf025b85e4e8b85a0d3b471533a
Erik Davtyan <erikdavtyan@gmail.com>
2018-02-26 11:37:15 -0700

    Login component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import Login from './Login';
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-        Hello world
+			<div style={styles.container}>
+				<Login />
 			</div>
 		);
 	}
 }
+
+const styles = {
+	container: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+};
 
 export default App;
---
+++ src/components/Login/index.js
@@ -0,0 +1,94 @@
+import React, { PureComponent } from 'react';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import { blue } from 'material-ui/colors';
+
+const Mode = {
+	login: 'login',
+	signup: 'signup',
+};
+
+class Login extends PureComponent {
+	state = {
+		username: '',
+		password: '',
+		passwordRepeat: '',
+		mode: Mode.login,
+	}
+	onChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	}
+	toggleMode = () => {
+		const mode = this.getToggleMode(this.state.mode);
+		this.setState({ mode });
+	}
+	getToggleMode = mode => (mode === Mode.login ? Mode.signup : Mode.login)
+	onSubmit = () => (this.state.mode === Mode.login ? this.login() : this.signup())
+	login = () => {
+		console.log();
+	}
+	signup = () => {
+		console.log('signup request here');
+	}
+	render() {
+		const { mode } = this.state;
+		const modeChange = this.getToggleMode(mode);
+		return (
+			<div style={styles.container}>
+				<div style={{ margin: 50, display: 'flex', flexDirection: 'column' }}>
+					<h1 style={styles.title}>{mode.toUpperCase()}</h1>
+					<TextField
+						name="username"
+						onChange={this.onChange}
+						value={this.state.username}
+						floatingLabelText="username"
+					/>
+					<TextField
+						name="password"
+						type="password"
+						onChange={this.onChange}
+						value={this.state.password}
+						floatingLabelText="password"
+					/>
+					{
+						mode === 'signup' &&
+							<TextField
+								name="passwordRepeat"
+								value={this.state.passwordRepeat}
+								onChange={this.onChange}
+							/>
+					}
+					<div style={{ display: 'flex' }}>
+						<Button
+							onClick={this.onSubmit}
+						>
+							<span>{mode}</span>
+						</Button>
+						<Button
+							label={modeChange}
+							onClick={this.toggleMode}
+						>
+							<span>{modeChange}</span>
+						</Button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+const styles = {
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		borderColor: blue[500],
+		borderStyle: 'solid',
+	},
+	title: {
+		textAlign: 'center',
+		color: blue[500],
+	},
+};
+
+export default Login;
+
