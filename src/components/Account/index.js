f79aed2a3faf7377735441d00b492118cd133fc7
Dax <graydax@gmail.com>
2018-03-01 23:58:59 -0700

    Style

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,5 +1,8 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { white } from 'material-ui/colors';
+import './style.css';
+import './images/user.jpg';
 
 const mapStateToProps = ({ user }) => ({ user });
 
@@ -7,11 +10,14 @@ class Account extends PureComponent {
 	render() {
 		const { user } = this.props;
 		return (
-			<div style={styles.container}>
-				<div style={styles.name}>
+			<div className="aside" style={styles.container}>
+				<div>
+					<img src="images/user.jpg" alt="Profile Picture" height="100" width="100"/>
+				</div>
+				<div className="accountInfo" style={styles.name}>
 					<span>{user.first_name} {user.last_name}</span>
 				</div>
-				<div>
+				<div className="accountInfo" >
 					<span>{user.email}</span>
 				</div>
 			</div>
@@ -24,14 +30,19 @@ const styles = {
 		display: 'flex',
 		flexDirection: 'column',
 		justifyContent: 'flex-start',
-		backgroundColor: 'green',
+		backgroundColor: 'white',
 		height: '100%',
+		borderColor: white,
+		borderStyle: 'solid',
+		backgroundColor: white,
 	},
 	name: {
-
+		textAlign: 'center',
+		color: white,
 	},
 	email: {
-
+		textAlign: 'center',
+		color: white,
 	},
 };
 
